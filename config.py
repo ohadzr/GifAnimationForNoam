ff927c8cee73492a5377c399a962d161a645a1f8
Ohad <ohadzr@gmail.com>
2019-05-04 13:22:03 +0300

    add config file adding video samples

---
+++ config.py
@@ -0,0 +1,24 @@
+
+ESC_KEY = '\x1b'
+
+SAMPLE_VID_BUNNY_LONG = r"samples\videos\SampleVideo_1280x720_30mb.mp4"
+SAMPLE_VID_BUNNY_SHORT = r"samples\videos\SampleVideo_1280x720_1mb.mp4"
+SAMPLE_VID_EARTH = r"samples/videos/earth_1280_10MG.mp4"
+SAMPLE_VID_SURFER = r"samples/videos/Surfer.mp4"
+
+
+ALL_VIDEOS = {
+    'main': SAMPLE_VID_BUNNY_SHORT,
+    'earth': SAMPLE_VID_EARTH,
+    'surfer': SAMPLE_VID_SURFER
+}
+
+
+CLICK_LOCATIONS = {
+
+}
+
+
+current_video_file = 'main'
+next_video_file = 'main'
+
--- main.py
+++ main.py
@@ -1,38 +1,54 @@
 import time
-import glob
 import cv2  # dependency
 
+from config import ESC_KEY, ALL_VIDEOS, current_video_file, next_video_file
 
 # mouse callback function
-def mouse_callback(event,x,y,flags,param):
+def mouse_callback(event, x, y, flags, param):
+    global next_video_file
+
     if event == cv2.EVENT_LBUTTONDOWN:
+        print('currently playing main, changing to second')
+        next_video_file = 'surfer'
         print((x, y))
 
-def play_video(video_file: str, frame_per_second=25, quit_key='q', fullscreen=True):
-    cap = cv2.VideoCapture(video_file)
+
+def play_video(video_files: dict, frames_per_second=25, quit_key=ESC_KEY, fullscreen=True):
+    # load main video first
+
+    # set as global for callback function
+    global current_video_file, next_video_file
+
+    cap = cv2.VideoCapture(video_files[current_video_file])
+
     if fullscreen:
         cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
     else:
         cv2.namedWindow("window")
 
-    cv2.setMouseCallback('window', mouse_callback)
+    cv2.setMouseCallback('window', mouse_callback, [video_files])
+    print(cv2.getWindowImageRect('window'))
 
     while cap.isOpened():
         ret, frame = cap.read()
 
-
-        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # cv2.imshow('frame', gray)
+        # it true, video hasn't reach end
         if ret:
             cv2.imshow('window', frame)
+
+        # else, end of video
         else:
             print('END OF VIDEO')
-            # load video again for loop
-            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            print("current:{} \nnext: {}".format(current_video_file, next_video_file))
+            if current_video_file == next_video_file:  # play video in loop
+                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            else:  # play different video
+                current_video_file = next_video_file
+                cap = cv2.VideoCapture(video_files[current_video_file])
 
-
-        if cv2.waitKey(frame_per_second) & 0xFF == ord(quit_key):
+        # if quit key was pressed, exit video (default: ESC key)
+        if cv2.waitKey(frames_per_second) & 0xFF == ord(quit_key):
             break
 
     cap.release()
@@ -43,9 +59,6 @@ if __name__ == "__main__":
     print("Starting program...")
     time.sleep(0.3)
 
-    sample_vid = r"samples\videos\SampleVideo_1280x720_30mb.mp4"  # video file path
-    sample_vid_short = r"samples\videos\SampleVideo_1280x720_1mb.mp4"  # video file path
-    samples_jpgs = glob.glob(r"samples/*jpg")
 
-    #play_video(sample_vid)
-    play_video(sample_vid_short, fullscreen=False)
+    # play_video(sample_vid)
+    play_video(ALL_VIDEOS, fullscreen=True)
